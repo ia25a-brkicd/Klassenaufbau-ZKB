1bf39ef72a25483d4575959367e8c77e16cd5e80
dario <brkicd@bzz.ch>
2025-10-28 14:48:58 +0100

    Uebungsaufgabe 04

--- src/main/java/exercise4/BankAccount.java
+++ src/main/java/exercise4/BankAccount.java
@@ -2,19 +2,44 @@ package exercise4;
 
 public class BankAccount {
 
-  public double balance;
-  public int fuelLevel;
-  public double mileage;
+  private String name;
+  private String surname;
+  private int accountNumber;
+  double balance;
+  private int limit;
 
-  public BankAccount(String max, String mustermann, int i, double v, int i1) {
+  public BankAccount(String name, String surname, int accountNumber, double balance, int limit) {
+      this.name = name;
+      this.surname = surname;
+      this.accountNumber = accountNumber;
+      this.balance = balance;
+      this.limit = limit;
   }
 
-  public void withdraw(int i) {
+  public void withdraw(double withdrawAmount) {
+      if (withdrawAmount > balance || withdrawAmount > limit) {
+          System.out.println("Insufficient funds!");
+      }
+      else{
+          balance -= withdrawAmount;
+      }
+
+
   }
 
-  public void deposit(int i) {
+  public void printBalance() {
+    System.out.println(balance);
   }
 
-  public void drive(int i) {
+
+
+
+
+  public String toString() {
+      return name + ", " + surname + ", " + accountNumber + ", " + balance +  ", " + limit;
   }
+
+    public void deposit(int i) {
+      balance += i;
+    }
 }
--- src/main/java/exercise4/Main.java
+++ src/main/java/exercise4/Main.java
@@ -2,6 +2,6 @@ package exercise4;
 
 public class Main {
     public static void main(String[] args) {
-
+        BankAccount newBankAccount = new BankAccount("Brkic","Dario",12332,600,1000);
     }
 }
